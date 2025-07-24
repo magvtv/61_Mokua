185d4247ed241b9b0bccaa48bc3844b345d40621
magvtv <magutujr@gmail.com>
2025-07-25 02:54:09 +0300

    feat: render coming soon page component only
    - had all the apps with routes and layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import AppLayout from './components/layout/AppLayout';
-import HomePage from './pages/HomePage';
-import PostDetailPage from './pages/PostDetailPage';
-import CategoryPage from './pages/CategoryPage';
-import AuthorsPage from './pages/AuthorsPage';
-import AuthorDetailPage from './pages/AuthorDetailPage';
-import SearchPage from './pages/SearchPage';
-import ContactPage from './pages/ContactPage';
-import SubmitPage from './pages/SubmitPage';
+import ComingSoonPage from './pages/ComingSoonPage';
 
 function App() {
-  return (
-    <AppLayout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/post/:slug" element={<PostDetailPage />} />
-        <Route path="/category/:slug" element={<CategoryPage />} />
-        <Route path="/authors" element={<AuthorsPage />} />
-        <Route path="/author/:slug" element={<AuthorDetailPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/submit" element={<SubmitPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </AppLayout>
-  );
+  return <ComingSoonPage />;
 }
 
 export default App;
