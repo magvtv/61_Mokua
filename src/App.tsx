b98f4db89fe7f4392c6f221c459bbdbfc90a52b9
magvtv <magutujr@gmail.com>
2025-08-06 15:10:25 +0300

    refactor: implement feature flag system with conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+// import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AppLayout from './components/layout/AppLayout';
+import HomePage from './pages/HomePage';
+import PostDetailPage from './pages/PostDetailPage';
+import AuthorsPage from './pages/AuthorsPage';
+import AuthorDetailPage from './pages/AuthorDetailPage';
+import CategoryPage from './pages/CategoryPage';
+import SearchPage from './pages/SearchPage';
+import ContactPage from './pages/ContactPage';
+import SubmitPage from './pages/SubmitPage';
 import ComingSoonPage from './pages/ComingSoonPage';
+import { isFeatureEnabled } from './utils/featureFlags';
 
 function App() {
-  return <ComingSoonPage />;
+  // If coming soon is enabled, show only that page
+  if (isFeatureEnabled('SHOW_COMING_SOON')) {
+    return <ComingSoonPage />;
+  }
+
+  // Main blog application with routing
+  return (
+    <AppLayout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/post/:slug" element={<PostDetailPage />} />
+        <Route path="/authors" element={<AuthorsPage />} />
+        <Route path="/author/:slug" element={<AuthorDetailPage />} />
+        <Route path="/category/:slug" element={<CategoryPage />} />
+        {isFeatureEnabled('ENABLE_SEARCH') && (
+          <Route path="/search" element={<SearchPage />} />
+        )}
+        <Route path="/contact" element={<ContactPage />} />
+        {isFeatureEnabled('ENABLE_SUBMISSIONS') && (
+          <Route path="/submit" element={<SubmitPage />} />
+        )}
+      </Routes>
+    </AppLayout>
+  );
 }
 
 export default App;
